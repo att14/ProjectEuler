23ee4eb932e5e6178ffd0660b11cec726b6e612d
Andrew Tribone <atribone@gmail.com>
2012-05-19 15:01:40 -0700

    12 and cleanup

--- 004/4.py
+++ 004/4.py
@@ -1,3 +1,8 @@
+"""A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
 def ispal(n):
 	s = str(n)
 	l = len(s)
@@ -10,4 +15,3 @@ for x in reversed(range(900, 999)):
 	for y in reversed(range(900, 999)):
 		if ispal(x * y):
 			print x * y
-	
